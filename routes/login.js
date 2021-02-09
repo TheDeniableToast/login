a1c59a69003e056ac1a436c631778c17dd166b0d
TheDeniableToast <filipnorberg02@outlook.com>
2021-02-09 11:18:59 +0100

    refactor: error msg to login

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET login form */
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-  res.render('login', {title: 'Honksida'})
+  res.render('login', {title: 'bannanklubens heem sidda 👌👌👌👌👌👌'})
+});
+
+/* POST login */
+router.post('/', function(req, res, next) {
+
+  console.log(req.body);
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (password == "bananpurre") {
+    res.send('Välkommen till Bananklubben grabben/grabbinnan 🍌🍌🍌🍌👌👌😘😘👍👍💋💋');
+  } else {
+    res.render(
+      'login',
+      {
+        title: 'bannanklubens heem sidda 👌👌👌👌👌👌',
+        error: 'FEL BITCH!'
+      }
+    );
+  }
+
 });
 
 module.exports = router;
